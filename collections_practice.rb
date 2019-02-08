c71f8ec31879cee8d1186ed031f4b3a16cb4f0e7
Eyal Toledano <eyaltoledano@me.com>
2019-02-08 22:41:23 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,69 +1,83 @@
-def begins_with_r(array) # Question 1
-  if array.any? {|item| !item.start_with?("r")}
-    false
-  elsif array.each { |item| item.start_with?("r")}
-    return true
-  end
-end
-
-def contain_a(array) # Question 2
-  new_array = []
-
-  array.each do |item|
-    if item.include?("a")
-      new_array << item
+def organize_schools(schools)
+  organized_schools = {}
+  schools.each do |name, location_hash|
+    location = location_hash[:location]
+    if organized_schools[location]
+      organized_schools[location] << name
+    else
+      organized_schools[location] = []
+      organized_schools[location] << name
     end
   end
-  new_array
+  organized_schools
 end
 
-def first_wa(array) # Question 3
-  first_element = []
+def begins_with_r(array)
+  flag = true
+  array.each do |element|
+    flag = false if element[0] != "r"
+  end
+  flag
+end
 
-  array.find do |item|
-    if item.to_s.include?("wa")
-      first_element << item
+def contain_a(array)
+  container = []
+  array.each do |element|
+    container << element if element.include?("a")
+  end
+  container
+end
+
+def remove_non_strings(array)
+  container = []
+  array.each do |element|
+    container << element if element.is_a?(String)
+  end
+  container
+end
+
+def first_wa(array)
+  first_wa = nil
+  array.each do |element|
+    if element.match(/wa/)
+      first_wa = element
+      break
     end
   end
-  first_element[0]
+  first_wa
 end
 
-def remove_non_strings(array) # Question 4
-  new_array = []
+def find_cool(array)
+  container = []
+  array.each do |element|
+    container << element if element[:temperature] == "cool"
+  end
+  container
+end
 
-  array.any? do |item|
-    if item.kind_of?(String)
-      new_array << item
+def count_elements(array)
+  array.each do |original_hash|
+    original_hash[:count] = 0
+    name = original_hash[:name]
+    array.each do |hash|
+      if hash[:name] == name
+        original_hash[:count] += 1
+      end
+    end
+  end.uniq
+end
+
+def merge_data(keys, values)
+  container = []
+  keys.each do |person_name|
+    name = person_name[:first_name]
+    values.each do |person_data|
+      if person_data[name]
+        merged_person = person_data[name]
+        merged_person[:first_name] = name
+        container << merged_person
+      end
     end
   end
-  new_array
-end
-
-def count_elements(array) # Question 5
-
-  counts = Hash.new 0
-  array.map do |word|
-    counts[word] += 1
-  end
-
-
-
-  #
-  # new_array = array.group_by(&:itself) # groups the same elements with each other
-  # key, value = new_array.first # sets the variables `key` and `value` to the first item in the new_array, which would be the name to be counted --> [{:name=>"blake"}, {:name=>"blake"}]. key and value are containing this
-  #
-  # new_array.map { |key, value| [key, key.merge(value.length)] }.to_h # on each of the items in the new_array, map first the name and second with the length of the `value` variable, which has two elements [{:name=>"blake"}, {:name=>"blake"}]
-  # #  {
-  # #    {:name=>"blake"}=>2,
-  # #    {:name=>"ashley"}=>1
-  # #  }
-end
-
-def merge_data(hash1, hash2) # Question 6
-end
-
-def find_cool(array) # Question 7
-end
-
-def organize_schools(array) # Question 8
+  container
 end
